cfcd23ccfad7ca25f6db8979b3d9b8e974c400e1
Peter Smrecek <petersmrecek@gmail.com>
2021-03-28 21:10:59 +0200

    Oprava chybneho refaktoru v urls.py

--- Zadanie1_app/urls.py
+++ Zadanie1_app/urls.py
@@ -9,8 +9,8 @@ urlpatterns = [
 
     path('v1/health', views.health, name="health"),
     path('', views.welcome, name="welcome"),
-    path('v1/ov/submissions/', views2.url_dispatcher, name="z3_get_print_pages"),
-    path('v1/ov/submissions', views2.url_dispatcher, name="z3_get_print_pages"),
+    path('v1/ov/submissions/', views2.url_dispatcher, name="get_print_pages"),
+    path('v1/ov/submissions', views2.url_dispatcher, name="get_print_pages"),
     path('v1/ov/submissions/<int:table_id>', views2.delete_row, name="delete_row"),
     path('v1/companies/', views3.z3_get_print_pages, name="Zadanie_3_GET"),
     path('v1/companies', views3.z3_get_print_pages, name="Zadanie_3_GET"),
